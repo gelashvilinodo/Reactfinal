4cc3768b975abce880028fe3bac43eaaaa9ddb8e
gelashvilinodo <166313791+gelashvilinodo@users.noreply.github.com>
2024-10-20 20:41:17 +0400

    trying fetch

--- reactfinal/src/App.js
+++ reactfinal/src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
 import "./reset.css";
 import User from "./components/User";
+import { useEffect } from "react";
+
+const api_token = "9XQsmODpojl6n8RFeZA1723UJBhzv2A9";
+const base_url = "http://localhost:8055/items/users";
 
 function App() {
+  useEffect(() => {
+    fetch(base_url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${api_token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log({ data }));
+  });
+
   return (
     <div className="cv_container">
       <div className="left_column">
--- reactfinal/src/components/User.jsx
+++ reactfinal/src/components/User.jsx
@@ -1,13 +1,13 @@
-function User() {
+function User({ name, lastname, profession }) {
   return (
     <div className="user_container">
       <div className="user_picture_div">
         <img src="" alt="" />
       </div>
       <div className="name_lastname">
-        <h4 className="user_name">nodo</h4>
-        <h2 className="user_lastname">gelashvili</h2>
-        <p className="user_proffesion">programmer</p>
+        <h4 className="user_name">{name}</h4>
+        <h2 className="user_lastname">{lastname}</h2>
+        <p className="user_proffesion">{profession}</p>
       </div>
     </div>
   );
