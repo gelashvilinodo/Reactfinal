b0843782ae5c4f91a18910e3e73d1674b53ffffb
gelashvilinodo <166313791+gelashvilinodo@users.noreply.github.com>
2024-10-31 03:47:59 +0400

    almost

--- reactfinal/src/components/CreateUser.jsx
+++
@@ -1,4 +0,0 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getUser, setUser } from "../cvReducer";
